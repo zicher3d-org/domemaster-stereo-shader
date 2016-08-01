7e81d5abeb61fdddc9a66dd885bb8f510be44ec6
Andrew Hazelden <andrew@andrewhazelden.com>
2016-08-01 08:41:58 -0300

    MR 3.14 Shader Makefile Updates

--- mentalray/original/latlong_stereo_2017/sources/windows64_compile.bat
+++ mentalray/original/latlong_stereo_2017/sources/windows64_compile.bat
@@ -2,6 +2,7 @@
 REM Andrew's Mental Ray Shader Compile Script 
 REM Version 2.0
 
+cd %USERPROFILE%"\Documents\GitHub\domemaster-stereo-shader\mentalray\original\latlong_stereo_2017\sources"
 
 ECHO. Started compiling the mental ray shader
 cl /c /O2 /MD /W3 -DWIN_NT -DBIT64 LatLong_Stereo.c  "C:\Program Files\NVIDIA Corporation\mentalray for Maya 2017\devkit\shaders\shader.c"

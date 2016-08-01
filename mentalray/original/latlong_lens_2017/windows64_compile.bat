f6829651ebf60a4b3b7b7941af8d0ff5990dfa18
Andrew Hazelden <andrew@andrewhazelden.com>
2016-08-01 08:41:24 -0300

    MR 3.14 Shader Makefile Updates

--- mentalray/original/latlong_lens_2017/windows64_compile.bat
+++ mentalray/original/latlong_lens_2017/windows64_compile.bat
@@ -1,10 +1,10 @@
 REM Andrew's Mental Ray Shader Compile Script 
 REM Version 2.0
 
-cd C:\Users\Andrew\Documents\GitHub\domemaster-stereo-shader\mentalray\original\latlong_lens_2017
+cd %USERPROFILE%"\Documents\GitHub\domemaster-stereo-shader\mentalray\original\latlong_lens_2017"
 
 ECHO. Started compiling the mental ray shader
-cl /c /O2 /MD /W3 -DWIN_NT -DBIT64 latlong_lens.cpp latlong_lens_dll.cpp "C:\Program Files\Autodesk\mentalrayForMaya2016Beta7\devkit\shaders\shader.c"
+cl /c /O2 /MD /W3 -DWIN_NT -DBIT64 latlong_lens.cpp latlong_lens_dll.cpp "C:\Program Files\NVIDIA Corporation\mentalray for Maya 2017\devkit\shaders\shader.c"
 
 ECHO. Linking Shader
 link /manifest /nologo /nodefaultlib:LIBC.LIB /OPT:NOREF /DLL /INCREMENTAL:NO /OUT:latlong_lens.dll latlong_lens_dll.obj latlong_lens.obj shader.obj

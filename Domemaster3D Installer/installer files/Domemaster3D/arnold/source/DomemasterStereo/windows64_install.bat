0753d2c29b072e11dcdc4f18fa8db9647fbc4c9b
Andrew Hazelden <andrew@andrewhazelden.com>
2016-08-04 07:53:35 -0300

    Arnold Makefile Updates

--- Domemaster3D Installer/installer files/Domemaster3D/arnold/source/DomemasterStereo/windows64_install.bat
+++ Domemaster3D Installer/installer files/Domemaster3D/arnold/source/DomemasterStereo/windows64_install.bat
@@ -2,6 +2,7 @@
 
 ECHO.
 ECHO Domemaster Shader For Arnold Installer
+ECHO Version 1.0 - 2016-08-04
 ECHO ---------------------------------------------------
 ECHO.
 
@@ -10,15 +11,15 @@ cd C:\solidangle\Arnold-DomemasterStereo-src\source\DomemasterStereo\
 ECHO Installing the shader Files:
 ECHO -------------------------------------
 ECHO DomemasterStereo.dll and DomemasterStereo.mtd
-copy DomemasterStereo.dll C:\solidangle\mtoadeploy\2015\shaders
-copy DomemasterStereo.mtd C:\solidangle\mtoadeploy\2015\shaders
+copy DomemasterStereo.dll C:\solidangle\mtoadeploy\2017\shaders
+copy DomemasterStereo.mtd C:\solidangle\mtoadeploy\2017\shaders
 
 ECHO. 
 ECHO. 
 ECHO Installing the AE Template File:
 ECHO -------------------------------------
 ECHO DomemasterStereoTemplate.py
-copy DomemasterStereoTemplate.py C:\solidangle\mtoadeploy\2015\scripts\mtoa\ui\ae
+copy DomemasterStereoTemplate.py C:\solidangle\mtoadeploy\2017\scripts\mtoa\ui\ae
 
 ECHO. 
 ECHO Installation Complete.

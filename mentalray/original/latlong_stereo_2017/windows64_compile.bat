9bd619877fb6bd4d587a723b14138ea2cb7bb2e7
Andrew Hazelden <andrew@andrewhazelden.com>
2016-09-16 20:56:09 -0300

    Maya 2017 Mental Ray Elliptical Filtering Update
    Updated mr 3.14 specific lens shader makefiles, re-compiled Mac,
    Windows, and Linux lens shader files, updated .mi include files

---
+++ mentalray/original/latlong_stereo_2017/windows64_compile.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM Andrew's Mental Ray Shader Compile Script 
+REM Version 2.0
+
+cd %USERPROFILE%"\Documents\GitHub\domemaster-stereo-shader\mentalray\original\latlong_stereo_2017\"
+
+ECHO. Started compiling the mental ray shader
+cl /c /O2 /MD /W3 -DWIN_NT -DBIT64 LatLong_Stereo.c  "C:\Program Files\NVIDIA Corporation\mentalray 3.14 for Maya 2017\devkit\shaders\shader.c"
+
+ECHO. Linking Shader
+link /manifest /nologo /nodefaultlib:LIBC.LIB /OPT:NOREF /DLL /INCREMENTAL:NO /OUT:LatLong_Stereo.dll LatLong_Stereo.obj shader.obj

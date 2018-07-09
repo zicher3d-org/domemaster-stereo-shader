1a990128fbd528d61dbec753f3947ea256edb86e
Andrew Hazelden <andrew@andrewhazelden.com>
2018-07-09 13:45:20 -0300

    vray 3.6 for 3ds Max 2018 Support

---
+++ Domemaster3D Installer/installer files/Domemaster3D/vray/source/3dsmax/vray 3.6 for 3ds Max 2018/vraydomemasterstereo/vraydomemasterstereo.h
@@ -0,0 +1,32 @@
+#ifndef __VRAYDOMEMASTERSTEREO_H__
+#define __VRAYDOMEMASTERSTEREO_H__
+
+namespace VRayDomemasterStereo
+{
+
+// Paramblock2 parameter list
+enum {
+  pb_camera,
+  pb_fov_angle,
+  pb_parallax_distance,
+  pb_separation,
+  pb_forward_tilt,
+  pb_tilt_compensation,
+  pb_vertical_mode,
+  pb_separation_map,
+  pb_head_turn_map,
+  pb_head_tilt_map,
+  pb_flip_x,
+  pb_flip_y,
+  pb_neck_offset,
+  pb_poles_corr,
+  pb_poles_corr_start,
+  pb_poles_corr_end,
+  pb_horiz_neck,
+  pb_parall_cams,
+  pb_fov
+};
+
+};
+
+#endif // #ifndef __VRAYDOMEMASTERSTEREO_H__
